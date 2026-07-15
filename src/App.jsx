484f64cab149445054e459efd97e13cd5af9202e
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-07-15 23:19:34 +0530

    project completed . added the navbar and make the four link functional without re-loading the page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import {Route , Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
+import Product_Page from './pages/Product_Page'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} /> 
         <Route path='/About' element={<About />} /> 
         <Route path='/Contact' element={<Contact />}/>
+        <Route path='/Product_Page' element={<Product_Page />} />
 
       </Routes>
     </div>
---
+++ src/pages/Product_Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product_Page = () => {
+  return (
+    <div>
+        <h1>Pucchu-kucchu</h1>
+    </div>
+  )
+}
+
+export default Product_Page
